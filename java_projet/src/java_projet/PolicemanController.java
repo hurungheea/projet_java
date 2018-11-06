a2a1ee964253347a137d2a140a0e30037cc5f467
hurungheea <achantelou@scinfe061.univ-pau.fr>
2018-11-06 18:52:34 +0100

    policeman fait

--- java_projet/src/java_projet/PolicemanController.java
+++ java_projet/src/java_projet/PolicemanController.java
@@ -6,13 +6,23 @@
 package java_projet;
 
 import com.FranckBarbier.Java._BCMS.BCMS;
+import com.pauware.pauware_engine._Exception.Statechart_exception;
+import java.awt.Color;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import java.util.regex.*;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.MenuButton;
+import javafx.scene.layout.Border;
 
 /**
  * FXML Controller class
@@ -21,11 +31,21 @@ import javafx.scene.input.MouseEvent;
  */
 public class PolicemanController implements Initializable
 {
-    private BCMS bcms;
     
-    public PolicemanController()
+    private BCMS bcms;
+    private int nbPvehicles;
+    
+    @FXML
+    private Label topLabel;
+    @FXML
+    private ChoiceBox<String> select;
+    @FXML
+    private Button backBtn;
+    
+    public PolicemanController() throws SQLException
     {
         this.bcms = gestionPF.getBcms();
+        this.nbPvehicles = this.bcms.get_police_vehicles().size();
     }
 
 
@@ -35,12 +55,22 @@ public class PolicemanController implements Initializable
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        // TODO
+        for(int i=1;i<= this.nbPvehicles;i++)
+        {
+            select.getItems().add(String.valueOf(i));
+        }
     }    
 
+     @FXML
+    private void quitApp()
+    {
+        gestionPF.quit();
+    }
+    
     @FXML
     private void backHome(MouseEvent event) throws IOException
     {
         gestionPF.changeScene("connectionPage.fxml");
     }
+
 }
